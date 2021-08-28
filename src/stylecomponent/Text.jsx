ab6898362d515ddf8b4e72fae4bdd4a1eae75903
DaeHui <77823790+choidaehui@users.noreply.github.com>
2021-08-28 16:21:02 +0900

    react: 안녕하세요

--- src/stylecomponent/Text.jsx
+++ src/stylecomponent/Text.jsx
@@ -6,12 +6,32 @@ import withStyles, { css } from './withStyles';
 
 class Text extends PureComponent {
   render() {
-    const { children, styles } = this.props;
+    const { 
+      children,
+      styles,
+      large,
+      xlarge,
+      small,
+      xsmall,
+      primary,
+      secondary,
+     } = this.props;
     return (
       // 자식 프로퍼티로 전달받은 노드를 span 엘리먼트 안에 출력
       // css() 함수로 엘리먼트에 default키에 정의된 스타일을 적용
       // css() 함수는 엘리먼트 속성 값을 객체형으로 반환하므로 전개연산자를 사용해 span 엘리먼트에 스타일을 적용
-      <span {...css(styles.default)}>
+      <span {...css(
+        styles.default,
+        xsmall && styles.xsmall,
+        // 프로퍼티 xsmall true일 경우 xsmall의 스타일을 추가. xsmall은 폰트 크기를 테마의 xs사이즈(10px)로 변경
+        small && styles.small,
+        large && styles.large,
+        xlarge && styles.xlarge,
+        secondary && styles.secondary,
+        primary && styles.primary,
+        // 프로퍼티 primary가 true일 경우 primary의 스타일을 추가, primary는 폰트 색상을 테마의 primary로 정의된
+        // 색상(#03a9f4)으로 변경
+        )}>
         {children}
       </span>
     );
@@ -20,6 +40,12 @@ class Text extends PureComponent {
 
 Text.PropTypes = {
   children: PropTypes.node.isRequired,
+  xsmall: PropTypes.bool,
+  small: PropTypes.bool,
+  large: PropTypes.bool,
+  xlarge: PropTypes.bool,
+  secondary: PropTypes.bool,
+  primary: PropTypes.bool,
 };
 
 export default withStyles(({ color, size }) => ({
@@ -28,7 +54,24 @@ export default withStyles(({ color, size }) => ({
     color: color.default,
     fontSize: size.md,
     // 스타일 속성을 낙타표기법(fontSize)으로 사용
-  }
-  ,
+  },
+  xlarge: {
+    fontSize: size.xg,
+  },
+  large: {
+    fontSize: size.lg,
+  },
+  small: {
+    fontSize: size.sm,
+  },
+  xsmall: {
+    fontSize: size.xs,
+  },
+  primary: {
+    color: color.primary,
+  },
+  secondary: {
+    color: color.secondary,
+  },
 }))(Text);
 // Text 컴포넌트에 withStyles() 함수를 적용
